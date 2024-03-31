d1533248ef2e37e2e15af941a9fec217af8a5738
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2024-03-31 17:27:40 -0500

    <AllProject /> and Finished Home Page

--- src/components/allProjects.jsx
+++ src/components/allProjects.jsx
@@ -7,35 +7,40 @@ import "./styles/allProjects.css"
 
 const AllProjects = () => {
     return (
-            <div className="project-section-container">
-                
-                <div className="projects-text">
-                    <div className="projects-title">
-                        Projects
-                    </div>
-                    <div className="projects-view-more">
-                        <a href="/">View More</a>
-                    </div>
-                </div>
+            <div className="project-section">
 
-                <div className="all-projects-container">
-                    {INFO.projects.map((project, index) => (
-                        <div className="all=projects-project" key={index}>
-                            <Project 
-                                title={project.title}
-                                description={project.description}
-                                languages={project.languages}
-                                githubLink={project.githubLink}
-                                deployedLink={project.deployedLink}
-                                projectLink={project.projectLink}
-                            />
+                <div className="project-flex">
+
+                    <div className="projects-text">
+
+                        <div className="projects-title">
+                            Projects
                         </div>
-                    ))}
+
+                        <div className="projects-view-more">
+                            <a href="/">View more</a>
+                        </div>
+
+                    </div>
+
+                    <div className="all-projects-container">
+                        {INFO.projects.map((project, index) => (
+                            <div className="all-projects-project" key={index}>
+                                <Project 
+                                    title={project.title}
+                                    description={project.description}
+                                    languages={project.languages}
+                                    githubLink={project.githubLink}
+                                    deployedLink={project.deployedLink}
+                                    projectLink={project.projectLink}
+                                />
+                            </div>
+                        ))}
+                    </div>
+
                 </div>
-
+                
             </div>
-
-            
     )
 }
 
